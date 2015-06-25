6bb3ef99e09a42cf1e9ca3807d2881fcc5ce420b
jdgwartney <davidg@boundary.com>
2015-06-25 11:21:13 -0700

    Check in latest

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,23 +3,29 @@ Package {
   allow_virtual => false,
 }
 
+package {'epel-release':
+  ensure => 'installed',
+  require => Exec['update-packages']
+}
+
+package {'jq':
+  ensure => 'installed',
+  require => Package['epel-release']
+}
+
 exec { 'update-packages':
   command => '/usr/bin/yum update -y',
   creates => '/vagrant/.locks/update-packages',
 }
 
-#
 # Add the GPG key to our system
-#
 exec { 'elasticsearch-repo-gpg-key':
   command => '/bin/rpm --import https://packages.elasticsearch.org/GPG-KEY-elasticsearch',
   creates => '/vagrant/.locks/add-elasticsearch-repo',
   require => Exec['update-packages']
 }
 
-#
 # Update our yum repository
-# TODO: Only perform this on Centos/Fedora/RedHat
 file { 'elasticsearch.repo':
   path    => '/etc/yum.repos.d/elasticsearch.repo',
   ensure  => file,
@@ -34,13 +40,9 @@ file { 'bash_profile':
   source  => '/vagrant/manifests/bash_profile'
 }
 
+node default {
 
-#
-# TODO: Multiple Nodes for Elastic Search
-#
-#node "boundary-elastic-search" {
-
- class { 'boundary':
+  class { 'boundary':
     token => '<api token>'
   }
 
@@ -48,24 +50,12 @@ file { 'bash_profile':
     distribution => 'jdk',
   }
 
-#  class { 'elasticsearch':
-#    ensure => 'present',
-#    require => [File['elasticsearch.repo'], Class['java']]
-#  }
-
   class { 'elasticsearch':
     config => { 'cluster.name' => 'boundary' },
     require => [File['elasticsearch.repo'], Class['java']]
   }
 
-  elasticsearch::instance { 'boundary-es-001':
-    config => { 'node.master' => 'true', 'node.data' => 'true'}
+  elasticsearch::instance { 'd':
+    config => { 'node.master' => 'true', 'node.data' => 'true', 'cluster.name' => 'boundary'},
   }
-  elasticsearch::instance { 'boundary-es-002':
-    config => { 'node.master' => 'false', 'node.data' => 'true'}
-  }
-  elasticsearch::instance { 'boundary-es-003':
-    config => { 'node.master' => 'false', 'node.data' => 'true'}
-  }
-
-#}
+}
